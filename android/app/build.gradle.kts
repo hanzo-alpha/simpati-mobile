050532c47b0e0abc5d7f87e59943d70cbe10e0b8
Hanzo Alpha <hansen.makangiras@hotmail.com>
2026-08-08 02:58:03 +0800

    feat: upgrade camera package to version 0.12.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.soppengkab.simpati_mobile"
+    namespace = "com.soppeng.simpati_mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.soppengkab.simpati_mobile"
+        applicationId = "com.soppeng.simpati_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
